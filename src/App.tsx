b62151685a8d8a5b8afb4d1880f37a200f0ec60e
Benjie Velarde <benjamin.velarde@disney.com>
2019-10-31 14:51:06 +0800

    task: Added Node-SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Suspense } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import logo from './logo.svg';
-import './App.css';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import './App.scss';
 
 const App: React.FC = () => {
   return (
     <Fragment>
       <CssBaseline />
-      <Container fixed>
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
-      </Container>
-    </Fragment>    
+      <Suspense fallback={<CircularProgress />}>
+        <Container fixed>
+          <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
+            <h1>Hello Material UI</h1>
+          </Typography>
+        </Container>
+      </Suspense>
+    </Fragment>
   );
 }
 
